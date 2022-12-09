fe0387216ecb36d0d7a61ef2b3e909903d0df4ab
akhildongre96 <akhildongre96@users.noreply.github.com>
2022-12-09 16:33:55 +0530

    First Commit

---
+++ practice/src/demo/ReposiDemo.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class ReposiDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("akhil");
+		System.out.println("dongre");
+
+	}
+
+}
