b19e89b2ee7aaf02b3cc1b1bccfd6e6196f2e4c5
akhildongre96 <akhildongre96@users.noreply.github.com>
2022-12-10 14:07:52 +0530

    second commit

--- practice/src/demo/ReposiDemo.java
+++ practice/src/demo/ReposiDemo.java
@@ -4,8 +4,19 @@ public class ReposiDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int arr [] = {10,20,30,40};
+		for(int i:arr) {
+		System.out.println(i);
+		}
+		String name="AKhiL";
+		int cout =name.toUpperCase().length();
+		int c=name.length();
+		System.out.println(c);
+		System.out.println(cout);
 		System.out.println("akhil");
 		System.out.println("dongre");
+		
 
 	}
 
